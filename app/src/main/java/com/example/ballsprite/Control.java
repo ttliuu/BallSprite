a7847ff09327bc2aab6c7551b3681228771e8266
ttliuu <tianwei.liu@outlook.com>
2020-05-15 17:15:43 -0400

    Moving around 4 directions

--- app/src/main/java/com/example/ballsprite/Control.java
+++ app/src/main/java/com/example/ballsprite/Control.java
@@ -32,31 +32,62 @@ public class Control {
         this.a = a;
     }
 
-    int xIndicator = 1;
+    int dIndicator = 1;
 
     public void update(Canvas c){
-        if (x > c.getWidth()){
-            xIndicator = -1;
-        }else if(x < 0){
-            xIndicator = 1;
+        if (x > c.getWidth()-500){
+            x = c.getWidth() - 500;
+            dIndicator = 2;
+        }
+        if (y > c.getHeight()-500){
+            y = c.getHeight()-500;
+            dIndicator = 3;
+
+        }
+        if (x < 0){
+            x = 0;
+            dIndicator = 4;
+        }
+        if(y < 00){
+            y = 0;
+            dIndicator = 1;
         }
 
-        if (xIndicator == 1){
-            x+=7;
-            y++;
+
+
+        if (dIndicator == 1){
+            x+=17;
         }
-        if ( xIndicator == -1){
-            x-=7;
-            y++;
+        if ( dIndicator == 2){
+            y+=17;
         }
+        if ( dIndicator == 3){
+            x-=17;
+        }
+        if ( dIndicator == 4){
+            y-=17;
+        }
+
+
         //System.out.println("testing 123");
     }
+
+    public void doTurning(Canvas c){
+
+
+
+
+
+
+    }
+
+
     public void draw(Canvas c){
         // System.out.println("testing 123");
         c.drawARGB(255,255,100,200);
         // c.drawBitmap(sprites,x,y,null);
-        Rect src = new Rect(0,0,400,400);
-        Rect dest = new Rect (x,y,x+100,y+120);
+        Rect src = new Rect(0,0,300,300);
+        Rect dest = new Rect (x,y,x+100,y+100);
         c.drawBitmap(sprites,src,dest,null);
 
     }
